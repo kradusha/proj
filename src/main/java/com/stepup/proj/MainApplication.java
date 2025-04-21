8a5ab56c779b007e590590b8a509aaf327654308
Nastasya Gavrilova <nastasya1301@yandex.ru>
2025-04-21 22:25:46 +0300

    Изменили логику doSomething на печать полезного текста

--- src/main/java/com/stepup/proj/MainApplication.java
+++ src/main/java/com/stepup/proj/MainApplication.java
@@ -8,8 +8,6 @@ public class MainApplication {
         System.out.println("555");
     }
     public static void doSomething() {
-        System.out.println(0);
-        System.out.println(1);
-        System.out.println(2);
+        System.out.println("Hello World!");
     }
 }

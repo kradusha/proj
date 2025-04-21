2755b20ac45252c9008e2572b949b414d2a429e1
Nastasya Gavrilova <nastasya1301@yandex.ru>
2025-04-21 17:34:21 +0300

    Изменена основная логика Добавлен класс Коробка

--- src/main/java/com/stepup/proj/MainApplication.java
+++ src/main/java/com/stepup/proj/MainApplication.java
@@ -3,6 +3,9 @@ package com.stepup.proj;
 public class MainApplication {
     public static void main(String[] args) {
         System.out.println("Hello World!");
-        System.out.println("333");
+        System.out.println("303");
+    }
+    public static void doSomething{
+        System.out.println(5);
     }
 }
---
+++ src/main/java/com/stepup/proj/Вох.java
@@ -0,0 +1,22 @@
+package com.stepup.proj;
+
+public class Вох {
+    private int size;
+
+    public int getSize() {
+        return size;
+    }
+
+    public void setSize(int size) {
+        this.size = size;
+    }
+
+    public Box() {
+
+    }
+
+    public Вох(int size) {
+        this.size = size;
+    }
+
+}
